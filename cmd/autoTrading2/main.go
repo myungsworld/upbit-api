48de73175fa4381bad31c452ed6dd5691e20b617
myungsworld <myungsworld@gmail.com>
2024-03-04 16:46:58 +0900

    deadlock mutex

--- cmd/autoTrading2/main.go
+++ cmd/autoTrading2/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"math"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 	"upbit-api/config"
 	"upbit-api/internal/api/candle"
+	"upbit-api/internal/connect"
+	"upbit-api/internal/models"
 )
 
 type info struct {
@@ -17,16 +21,35 @@ type info struct {
 	TradeAverage float64 // 종가 평균
 	HighAverage  float64 // 고가 평균
 
-	LowTradeGap  float64 // 저가 - 종가 차이
-	HighTradeGap float64 // 고가 - 종가 차이
+	LowTradeGap     float64 // 저가 - 종가 차이
+	TradeOpeningGap float64 // 종가 대비 시작가
+	HighTradeGap    float64 // 고가 - 종가 차이
 
 	OpeningPrice float64 // 금일 시작가
+}
+
+func (i info) String() string {
+
+	result := fmt.Sprintf(
+		`저점평균: %0.2f
+종가 평균: %0.2f
+고가 평균: %0.2f
+금일 시작가 : %f
+저점대비 종가: %0.2f%%
+종가대비 시가: %0.2f%%
+고점대비 종가: %0.2f%%`, i.LowAverage, i.TradeAverage, i.HighAverage, i.OpeningPrice, i.LowTradeGap, i.TradeOpeningGap, i.HighTradeGap)
+
+	return result
+
 }
 
 // 이전 데이터 가져오는 기준일
 const count = 4
 
-var previousMarketInfo map[string]info
+var (
+	previousMarketInfo  map[string]info
+	previousMarketMutex sync.RWMutex
+)
 
 func main() {
 
@@ -36,16 +59,69 @@ func main() {
 	// 1일마다 리셋 ( 한국시각 9시 )
 	go reset()
 
-	oneSecTicker := time.NewTicker(time.Second)
+	socketOpenTicker := time.NewTicker(time.Second * 2)
 
 	go func() {
 		for {
 			select {
-			case <-oneSecTicker.C:
-				fmt.Println(len(previousMarketInfo))
+			// 소켓 시세 수신
+			case <-socketOpenTicker.C:
+
+				conn := connect.Socket(config.Ticker)
+				for {
+					_, message, err := conn.ReadMessage()
+					if err != nil {
+						break
+					}
+
+					ticker := models.Ticker{}
+					if err := json.Unmarshal(message, &ticker); err != nil {
+						panic(err)
+					}
+
+					previousMarketMutex.Lock()
+					if data, ok := previousMarketInfo[ticker.Code]; ok {
+
+						fmt.Println(ticker.Code)
+						fmt.Println(data)
+
+						fmt.Println(ticker.Code, "삭제")
+						delete(previousMarketInfo, ticker.Code)
+
+						// opening Price 가 저점의 평균보다 낮으면 매수 안함 ( 오늘 갑자기 존나 내려간 경우 혹은 어제 많이 내려서 마감한 경우)
+
+						// opening Price 가 고점의 평균보다 높으면 매수 안함 ( 오늘 갑자기 존나 올라온 경우 혹은 어제 많이 올라서 마감한 경우)
+
+						// 하루전에 급격히 오른경우 다시 봐야함
+
+						// 저점의 평균으로 왔을때 매수
+
+						// 고점과 저점의 퍼센트 차이 구하고 그 반 퍼센트의 수익이 난다면 매도
+
+					}
+					previousMarketMutex.Unlock()
+
+				}
+
+				fmt.Println("여기는 for 문 밖")
+
 			}
 		}
 	}()
+
+	//go func() {
+	//	for {
+	//		select {
+	//		case val, ok := <-golChan:
+	//			if !ok {
+	//				fmt.Println(val, ok)
+	//			} else {
+	//				fmt.Println(val, ok)
+	//				time.Sleep(time.Hour)
+	//			}
+	//		}
+	//	}
+	//}()
 
 	// opening Price 가 저점의 평균보다 낮으면 매수 안함 ( 오늘 갑자기 존나 내려간 경우 혹은 어제 많이 내려서 마감한 경우)
 
@@ -63,6 +139,13 @@ func main() {
 
 func reset() {
 
+	//// 매일 9시 티커 설정
+	//now := time.Now()
+	//
+	//resetTime := now.Truncate(24 * time.Hour).Add(time.Hour * 24).Add(time.Second)
+	//
+	//setTicker = time.NewTicker(resetTime.Sub(now))
+
 	setTicker := time.NewTicker(time.Second)
 
 	//
@@ -76,18 +159,19 @@ func reset() {
 			// 상태값 담을 map
 			previousMarketInfo = make(map[string]info)
 
+			st := time.Now()
+
 			// 지난 n일의 저점과 고점의 평균을 구함 ( 하루동안 상태값으로 남겨놓음 )
 			{
-				// 1초에 20번
 
 				for _, market := range config.Markets {
 
-					// API 갯수 제한
-					time.Sleep(80 * time.Millisecond)
-
-					//if market != "KRW-GLM" {
+					//if market != "KRW-SOL" && market != "KRW-GRT" {
 					//	continue
 					//}
+
+					//API 갯수 제한
+					time.Sleep(100 * time.Millisecond)
 
 					can := candle.Market(market)
 
@@ -112,33 +196,36 @@ func reset() {
 					tradeAverage = tradeAverage / (count - 1)
 					highAverage = highAverage / (count - 1)
 					lowTradeGap := math.Trunc((lowAverage/tradeAverage-1)*10000) / 100
+					tradeOpeningGap := math.Trunc((tradeAverage/openingPrice-1)*10000) / 100
 					highTradeGap := math.Trunc((highAverage/tradeAverage-1)*10000) / 100
 
+					previousMarketMutex.Lock()
 					previousMarketInfo[market] = info{
 						lowAverage,
 						tradeAverage,
 						highAverage,
 						lowTradeGap,
+						tradeOpeningGap,
 						highTradeGap,
 						openingPrice,
 					}
-
-					fmt.Print("저점 평균:", lowAverage)
-					fmt.Print("종가 평균:", tradeAverage)
-					fmt.Print("고가 평균:", highAverage)
-					fmt.Print("금일 시작가:", openingPrice)
-					fmt.Print("저점대비 종가:", lowTradeGap)
-					fmt.Println("고점대비 종가:", highTradeGap)
+					previousMarketMutex.Unlock()
 				}
 
 			}
-			// 매일 9시 티커 설정
-			now := time.Now()
 
-			resetTime := now.Truncate(24 * time.Hour).Add(time.Hour * 24).Add(time.Second)
+			et := time.Now()
 
-			setTicker = time.NewTicker(resetTime.Sub(now))
+			fmt.Println("실행시간 :", et.Sub(st))
 
+			//// 매일 9시 티커 설정
+			//now := time.Now()
+			//
+			//resetTime := now.Truncate(24 * time.Hour).Add(time.Hour * 24).Add(time.Second)
+			//
+			//setTicker = time.NewTicker(resetTime.Sub(now))
+
+			setTicker = time.NewTicker(time.Second * 60)
 			log.Println("데이터 초기화 끝")
 
 		}
--- cmd/test/main.go
+++ cmd/test/main.go
@@ -2,35 +2,48 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 	"upbit-api/config"
 )
 
 func main() {
 
-	for i := 0; i < 20; i++ {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Println("혁두캔두잇")
-	}
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
 
-	//// 전날대비 퍼센트 비교
-	//conn := connect.Socket(config.Ticker)
-	////tickers := make(map[string]models.Ticker, 0)
-	////arrTickers := make([]models.Ticker, 0)
-	////
-	//for {
-	//	_, message, err := conn.ReadMessage()
-	//	if err != nil {
-	//		break
-	//	}
-	//
-	//	ticker := models.Ticker{}
-	//	if err := json.Unmarshal(message, &ticker); err != nil {
-	//		panic(err)
-	//	}
-	//
-	//	fmt.Println(ticker)
-	//}
+	oneChan := make(chan int, 1)
+	block := make(chan bool)
+
+	var a int
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			a++
+			fmt.Println("a 값:", a)
+			go func() {
+
+				oneChan <- a
+
+			}()
+		}
+	}()
+
+	go func() {
+		for {
+			select {
+			case val, ok := <-oneChan:
+				block <- true
+				fmt.Println("뽑아온 값:", val, ok)
+				time.Sleep(time.Second * 5)
+
+			}
+		}
+	}()
+
+	<-stopChan
 
 }
 
