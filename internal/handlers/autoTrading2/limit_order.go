973e872dbf0cce9286735485262f5a0c8c77936a
myungsworld <myungsworld@gmail.com>
2024-04-29 15:39:41 +0900

    현재가 업데이트

--- internal/handlers/autoTrading2/limit_order.go
+++ internal/handlers/autoTrading2/limit_order.go
@@ -35,6 +35,9 @@ func LimitOrder() {
 			PreviousMarketMutex.Lock()
 			if info, ok := PreviousMarketInfo[ticker.Code]; ok {
 
+				// 현재가와 종가평균 대비 현재가 퍼센트 다시 업데이트
+				tradePriceUpdate(ticker, info)
+
 				//TODO : 시간이 새벽 6시 이후인 경우에는 매수체결 안검
 				// 이유 : 초기화까지 3시간도 안남아서 굳이 할 필요가 없다고 느끼기 때문
 				//if time.Now().UTC().Hour() >= 21 {
@@ -43,13 +46,17 @@ func LimitOrder() {
 				//	continue
 				//}
 
-				// opening Price(시작가) 가 저점의 평균보다 낮으면 매수 안함 ( 오늘 갑자기 많이 내려간 경우 혹은 어제 많이 내려서 마감한 경우)
 				// opening Price 가 고점의 평균보다 높으면 매수 안함 ( 오늘 갑자기 많이 올라온 경우 혹은 어제 많이 올라서 마감한 경우)
-				if info.LowAverage > info.OpeningPrice || info.HighAverage < info.OpeningPrice {
+				if info.HighAverage < info.OpeningPrice {
 					delete(PreviousMarketInfo, ticker.Code)
 					PreviousMarketMutex.Unlock()
 					continue
 				}
+
+				////opening Price(시작가) 가 저점의 평균보다 낮으면 매수 안함 ( 오늘 갑자기 많이 내려간 경우 혹은 어제 많이 내려서 마감한 경우)
+				//if info.LowAverage > info.OpeningPrice {
+				//
+				//}
 
 				// 금일 시작가와 종가의 평균 편차가 0.5% 내외면 모니터링 진입
 				if math.Abs(info.CloseTradingGap) < 0.5 {
@@ -123,4 +130,18 @@ func LimitOrder() {
 
 	}
 
+}
+
+func tradePriceUpdate(ticker models.Ticker, info Info) {
+	newCloseTradingGap := math.Trunc((ticker.TradePrice/info.TradeAverage-1)*10000) / 100
+	PreviousMarketInfo[ticker.Code] = Info{
+		LowAverage:      info.LowAverage,
+		TradeAverage:    info.TradeAverage,
+		HighAverage:     info.HighAverage,
+		LowTradeGap:     info.LowTradeGap,
+		CloseTradingGap: newCloseTradingGap,
+		HighTradeGap:    info.HighTradeGap,
+		OpeningPrice:    info.OpeningPrice,
+		TradePrice:      ticker.TradePrice,
+	}
 }
